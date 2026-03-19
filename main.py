b4a92ad20a2141a24f7d231aea5efdbc05b38c1e
Bryan <buungoc1805@gmail.com>
2026-03-19 14:48:15 +0700

    Create basic window game

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QPushButton, QWidget
+from core.game_state import GameState
+
+class BasicMainWindow(QMainWindow):
+    def __init__(self, state: GameState):
+        super().__init__()
+        self.state = state
+        self.setWindowTitle("Pandemic Command - Pre-Alpha M0")
+        self.resize(400, 200)
+
+        # UI Components
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.layout = QVBoxLayout(self.central_widget)
+
+        self.info_label = QLabel(self._get_state_text())
+        self.layout.addWidget(self.info_label)
+
+        self.next_turn_btn = QPushButton("Next Turn (1 Week)")
+        self.next_turn_btn.clicked.connect(self.handle_next_turn)
+        self.layout.addWidget(self.next_turn_btn)
+
+    def _get_state_text(self) -> str:
+        return (f"Date: {self.state.current_date.strftime('%d-%m-%Y')} (Turn {self.state.turn})\n"
+                f"Infectious: {self.state.infectious}\n"
+                f"Budget: ${self.state.budget_mil_usd}M\n"
+                f"Satisfaction: {self.state.satisfaction_index}%")
+    
+    def handle_next_turn(self):
+        self.state.advance_turn()
+        self.info_label.setText(self._get_state_text()) # Update UI
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    # Init model
+    initial_state = GameState()
+
+    # Init View
+    window = BasicMainWindow(initial_state)
+    window.show()
+
+    sys.exit(app.exec())
